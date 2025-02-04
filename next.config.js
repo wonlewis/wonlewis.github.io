db65081ab1102b391f0c0e87ad59d72eb32fa19b
Lewis Won <lewis.won@gmail.com>
2025-02-04 19:35:51 +0800

    updated env file

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isProd = process.env.NODE_ENV === 'production';
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+        unoptimized: true, // Disable default image optimization
+    },
+    assetPrefix: isProd ? '/wonlewis.github.io/' : '',
+    basePath: isProd ? '/wonlewis.github.io' : '',
+    output: 'export'
+};
 
-module.exports = nextConfig;
+export default nextConfig;
